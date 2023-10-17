3b9aec4200701ac4f857ae8724a259376fafc4ad
Ricardo FRanco Hernandez <ricardo.hern@icloud.com>
2023-10-17 14:50:49 -0300

    finished the first test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import MovieList from './components/MovieList.vue'
+
 </script>
 
 <template>
@@ -8,12 +10,12 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <MovieList />
   </main>
 </template>
 
